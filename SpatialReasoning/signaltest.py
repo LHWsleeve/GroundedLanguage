dd1dad380492cac809e534645a4a6fb957a9b1f0
sleeve <1421147715@qq.com>
2020-06-29 14:16:16 +0800

    2020.5.6
    我也不记得这是啥。。

---
+++ SpatialReasoning/signaltest.py
@@ -0,0 +1,6 @@
+import  re
+
+
+String1 = 'After receiving the xxxxx signal, Move block SRI up until it touches the block above it, ' \
+         'with the Toyota symbol.'
+String2 = 'Receive a abc signal before moving the twitter block , then align it with the SRI.'
